2fbe6835deb69b13a317efb4cee5555e78cce3ac
Aleksandr Volkov <aev7823@gmail.com>
2025-12-09 03:53:09 +0700

    Донастроил auth0

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Auth0ProviderWithNavigate from './user/auth0/auth0-provider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Auth0ProviderWithNavigate>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </Auth0ProviderWithNavigate>
   )
 }
 
---
+++ src/user/auth0/auth0-provider.tsx
@@ -0,0 +1,29 @@
+import { Auth0Provider } from "@auth0/auth0-react";
+import type { ReactNode } from "react";
+
+interface Auth0ProviderWithNavigateProps {
+  children: ReactNode;
+}
+
+const Auth0ProviderWithNavigate: React.FC<Auth0ProviderWithNavigateProps> = ({
+  children,
+}) => {
+  return (
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID_SPA} // SPA client
+      useRefreshTokens={true}
+      cacheLocation="localstorage"
+      authorizationParams={{
+        redirect_uri: window.location.origin + "/auth-callback",
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+        scope: "openid profile email",
+        connection: "Username-Password-Authentication",
+      }}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+
+export default Auth0ProviderWithNavigate;
