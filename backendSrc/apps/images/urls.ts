fe1515c80e20d401ac46e74c88d8e37bc925afd2
Aleksandr Volkov <aev7823@gmail.com>
2025-12-15 01:47:52 +0700

    Добавлен запрос на скачивание файла

--- backendSrc/apps/images/urls.ts
+++ backendSrc/apps/images/urls.ts
@@ -1,8 +1,10 @@
 import type { RouterType } from '../../router'
+import { downloadImageApi } from './api/downloadImageApi'
 import { GetImagesApi } from './api/getImagesApi'
 import { PostImageApi } from './api/postImageApi'
 
 export default function registerImagesRoutes(router: RouterType) {
   router.get("/api/images/", GetImagesApi)
   router.post("/api/images/upload", PostImageApi)
+  router.get("/image/:key/download", downloadImageApi)
 }
