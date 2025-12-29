35a8344d8333835d8e4c8e967bf0c3223a46bd59
Aleksandr Volkov <aev7823@gmail.com>
2025-12-30 03:11:29 +0700

    Добавлено позиционирование sticky

--- src/pages/user/StickersPage.tsx
+++ src/pages/user/StickersPage.tsx
@@ -11,10 +11,10 @@ export const StickersPage = ({ category }: CategoryPageProps) => {
   const filteredImages = useFilterPage({ category })
 
   return (
-    <div className="font-display">
+    <div className="font-display bg-black">
+      <Header />
       <div className="relative flex h-auto min-h-screen w-full flex-col bg-background-light dark:bg-background-dark overflow-x-hidden">
         <div className="layout-container flex h-full grow flex-col">
-          <Header />
           <div className="px-4 sm:px-10 md:px-20 lg:px-40 flex flex-1 justify-center py-5">
             <div className="layout-content-container flex flex-col w-full max-w-screen-xl flex-1">
               {/* <!-- PageHeading --> */}
--- src/pages/user/WallpapersPage.tsx
+++ src/pages/user/WallpapersPage.tsx
@@ -10,52 +10,54 @@ export function WallpapersPage({ category }: CategoryPageProps) {
   const filteredImages = useFilterPage({ category })
 
   return (
-    <div className="relative flex min-h-screen w-full flex-col group/design-root overflow-x-hidden font-display bg-black">
-      <div className="layout-container flex h-full grow flex-col">
-        {/* HEADER */}
-        <Header />
-        {/* MAIN */}
-        <main className="flex flex-1 justify-center py-5">
-          <div className="layout-content-container flex flex-col w-full max-w-7xl px-4 sm:px-10">
-            <div className="flex flex-wrap justify-between gap-3 p-4">
-              <div className="flex min-w-72 flex-col gap-3">
-                <p className="text-slate-900 dark:text-white text-4xl font-black leading-tight tracking-[-0.033em]">
-                  Wallpapers
-                </p>
-                <p className="text-slate-500 dark:text-slate-400 text-base font-normal leading-normal">
-                  Explore an endless collection of high-quality wallpapers for your desktop.
-                </p>
-              </div>
-            </div>
-            <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4 p-4">
-              {filteredImages.map(img => (
-                <div
-                  key={`${img.type}-${img.id}`}
-                  onClick={() => {
-                    dispatch(openImageModal(img))
-                  }}
-                  className="relative group image-card overflow-hidden rounded-lg">
-                  <img
-                    className="w-full h-full object-cover aspect-[3/4] transition-transform duration-300 group-hover:scale-105"
-                    data-alt="Abstract swirling colors of pink and blue paint in water."
-                    src={img.url}
-                  />
+    <div className="font-display bg-black">
+      <Header />
+      <div className="relative flex min-h-screen w-full flex-col group/design-root overflow-x-hidden font-display bg-black">
+        <div className="layout-container flex h-full grow flex-col">
+          {/* HEADER */}
+          {/* MAIN */}
+          <main className="flex flex-1 justify-center py-5">
+            <div className="layout-content-container flex flex-col w-full max-w-7xl px-4 sm:px-10">
+              <div className="flex flex-wrap justify-between gap-3 p-4">
+                <div className="flex min-w-72 flex-col gap-3">
+                  <p className="text-slate-900 dark:text-white text-4xl font-black leading-tight tracking-[-0.033em]">
+                    Wallpapers
+                  </p>
+                  <p className="text-slate-500 dark:text-slate-400 text-base font-normal leading-normal">
+                    Explore an endless collection of high-quality wallpapers for your desktop.
+                  </p>
                 </div>
-              ))}
-            </div>
-            {isOpen && <ModalDownload onClose={() => dispatch(closeImageModal())} file={selectedImage!} />}
-            <div className="flex flex-col gap-3 p-4 my-10">
-              <div className="flex gap-6 justify-between">
-                <p className="text-slate-900 dark:text-white text-base font-medium leading-normal">
-                  Loading new wallpapers...
-                </p>
               </div>
-              <div className="rounded-full bg-slate-200 dark:bg-slate-700 overflow-hidden">
-                <div className="h-2 rounded-full bg-primary animate-pulse" style={{ width: '100%' }}></div>
+              <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4 p-4">
+                {filteredImages.map(img => (
+                  <div
+                    key={`${img.type}-${img.id}`}
+                    onClick={() => {
+                      dispatch(openImageModal(img))
+                    }}
+                    className="relative group image-card overflow-hidden rounded-lg">
+                    <img
+                      className="w-full h-full object-cover aspect-[3/4] transition-transform duration-300 group-hover:scale-105"
+                      data-alt="Abstract swirling colors of pink and blue paint in water."
+                      src={img.url}
+                    />
+                  </div>
+                ))}
+              </div>
+              {isOpen && <ModalDownload onClose={() => dispatch(closeImageModal())} file={selectedImage!} />}
+              <div className="flex flex-col gap-3 p-4 my-10">
+                <div className="flex gap-6 justify-between">
+                  <p className="text-slate-900 dark:text-white text-base font-medium leading-normal">
+                    Loading new wallpapers...
+                  </p>
+                </div>
+                <div className="rounded-full bg-slate-200 dark:bg-slate-700 overflow-hidden">
+                  <div className="h-2 rounded-full bg-primary animate-pulse" style={{ width: '100%' }}></div>
+                </div>
               </div>
             </div>
-          </div>
-        </main>
+          </main>
+        </div>
       </div>
     </div>
   )
