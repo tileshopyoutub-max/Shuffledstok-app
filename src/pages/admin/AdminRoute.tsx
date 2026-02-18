f699768b367e7622140d093fe7ea39c7c0752111
KulichO <oaosokina@yandex.ru>
2026-02-18 14:20:24 +0300

    сняла защиту с админки

--- src/pages/admin/AdminRoute.tsx
+++ src/pages/admin/AdminRoute.tsx
@@ -1,26 +1,5 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
-
-const ADMIN_ROLE = "admin";
-const ROLES_CLAIM = "https://shuffledstock-app.com/roles";
+import { Outlet } from "react-router-dom";
 
 export const AdminRoute = () => {
-  // const { isAuthenticated, isLoading, user, loginWithRedirect } = useAuth0()
-
-  // if (isLoading) {
-  //   return <div>Loading...</div>
-  // }
-
-  // if (!isAuthenticated) {
-  //   loginWithRedirect()
-  //   return null
-  // }
-
-  // const roles: string[] = user?.[ROLES_CLAIM] || []
-
-  // if (!roles.includes(ADMIN_ROLE)) {
-  //   return <Navigate to="/" replace />
-  // }
-
   return <Outlet />;
 };
